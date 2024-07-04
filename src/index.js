0d61a56b49f603aa843261a46687fc167f3c2919
Bennita-nnita <165434996+Bennita-nnita@users.noreply.github.com>
2024-07-04 21:19:33 +0000

    updated js to ensure ai responsiveness

--- src/index.js
+++ src/index.js
@@ -13,16 +13,7 @@ function generateRecipe(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "a4t22d03404203ac1b80ofbb2b04aaf5";
-    let context = `
-        You are an expert in West African cuisine. Your mission is to generate a simple, easy-to-follow recipe 
-        from any part of West Africa based on the ingredient provided in the user-instructions. 
-        The recipe should be written in basic HTML and formatted with each line separated by a <br/> tag. 
-        Start the recipe by listing the ingredients needed for the dish. 
-        Follow this with the heading 'Process', and then list each step of the process, 
-        each new line or full stop is seperated with a </br>. The process should not exceed 10 lines and should be very easy to follow. 
-        Do not include a title for the recipe. 
-        End the recipe with the signature 'Nnita's AI recipes' inside a <strong> element.
-    `;
+    let context = `You are an expert in West African cuisine. Your mission is to generate a simple, easy-to-follow recipe. From any part of West Africa based on the ingredient provided in the user-instructions. The recipe should be written in basic HTML and formatted with each line separated by a </br> tag. Start the recipe by listing the ingredients needed for the dish. then on the next line follow this with the heading 'Process', and then list each step of the process, each new line or full stop is seperated with a </br>. The process should not exceed 10 lines and should be very easy to follow.Each step of the process should begin on a new line as well. Do not include a title for the recipe. End the recipe with the signature 'Nnita's AI recipes' inside a <strong> element.`;
     let prompt = `User instructions: Generate a West African recipe highlighting ${instructionsInput.value} ingredient`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
